8aabfe9870c69601710ba0be4d8797d29a567671
Manjunath Kudlur <keveman@gmail.com>
2024-10-23 16:17:01 -0700

    Expose load_audio from moonshine namespace

--- moonshine/__init__.py
+++ moonshine/__init__.py
@@ -4,4 +4,4 @@ from .version import __version__
 ASSETS_DIR = Path(__file__).parents[0] / "assets"
 
 from .model import load_model, Moonshine
-from .transcribe import transcribe, benchmark, load_tokenizer
+from .transcribe import transcribe, benchmark, load_tokenizer, load_audio
